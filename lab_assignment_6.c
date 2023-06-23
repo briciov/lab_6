bb35adcdb8c239bc04ab89c186a88c9284001b4e
Mabricio Ventura <mabricioventuraknights.ucf.edu>
2023-06-23 19:16:39 -0400

    Added changes

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h> 
 
-int search(int numbers[], int low, int high, int value) 
+    int search(int numbers[],int low, int high, int value);
+    int search(int numbers[],int low, int high, int value)
 {
-	return -1;
+    if (low > high) return -1;
+
+    int middle = (low+high)/2;
+
+    if (value < numbers[middle])
+        return search (numbers, low, middle-1, value);
+
+    else if (value > numbers[middle])
+        return search (numbers, middle+1, high, value);
+
+    else
+        return middle;
+
 }
 
 void printArray(int numbers[], int sz)
@@ -58,3 +72,5 @@ int main(void)
 
 	fclose(inFile);
 }
+
+
